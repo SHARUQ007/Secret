1ef7515cb60cc0491c35736ecb65308666e70da9
SHARUQ007 <58297752+SHARUQ007@users.noreply.github.com>
2020-10-14 20:11:14 +0530

    bcrypt

--- app.js
+++ app.js
@@ -4,7 +4,8 @@ const express=require("express");
 const bodyParser=require("body-parser");
 const ejs=require("ejs");
 const mongoose =require("mongoose");
-const md5=require("md5");
+const bcrypt=require("bcrypt");
+const saltrounds=10;
 
 const app=express();
 
@@ -35,9 +36,10 @@ app.get("/register",function(req,res){
 })
 
 app.post("/register",function(req,res){
+  bcrypt.hash(req.body.password,saltrounds,function(err,hash){
   const newUser = new User({
     email:req.body.username,
-    password:md5(req.body.password)
+    password:hash
   })
   newUser.save(function(err){
     if(err){
@@ -45,23 +47,25 @@ app.post("/register",function(req,res){
     }else{
       res.render("secrets");
     }
-  })
-})
+  });
+});
+});
 app.post("/login",function(req,res){
 const username=req.body.username;
-const password=md5(req.body.password);
+const password=(req.body.password);
 User.findOne({email:username},function(err,foundUser){
   if(err){
     console.log(err);
   }else{
     if(foundUser){
-      if(foundUser.password === password){
+      bcrypt.compare(password,foundUser.password, function(err, result) {
+        if(result===true)
         res.render("secrets");
-      }
+});
     }
   }
-})
-})
+});
+});
 
 
 app.listen(3000,function(){
